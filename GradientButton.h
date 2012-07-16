99c093b9b2f1a0c62b9ee160b43187f03c510afb
Tommy Goode <tommy@tgoode.com>
2012-07-16 02:23:20 -0400

    Adding the initial implementation of a GradientButton. The button makes a gradient with a high color and a low color. The class exposes the 'setupButton' method to allow subclasses to easily set all button properties at creation time.

---
+++ GradientButton.h
@@ -0,0 +1,18 @@
+//
+// GradientButton.h
+//
+// Created by Thomas Goode on 7/16/12.
+// Copyright (c) 2012 Thomas Goode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface GradientButton : UIButton
+
+@property (nonatomic, strong) UIColor *highColor;
+@property (nonatomic, strong) UIColor *lowColor;
+
+-(void)setupButton;
+
+@end
---
+++ GradientButton.m
@@ -0,0 +1,79 @@
+//
+// GradientButton.m
+//
+// Created by Thomas Goode on 7/16/12.
+// Copyright (c) 2012 Thomas Goode. All rights reserved.
+//
+
+#import "GradientButton.h"
+
+@interface GradientButton ()
+
+@property (nonatomic, strong) CAGradientLayer *gradientLayer;
+
+-(void)setupButton;
+-(void)configureButtonLayer;
+-(void)updateButton;
+
+@end
+
+@implementation GradientButton
+
+@synthesize highColor = _highColor;
+@synthesize lowColor = _lowColor;
+@synthesize gradientLayer = _gradientLayer;
+
+#pragma mark init and setup methods
+-(void)awakeFromNib {
+	[self setupButton];
+}
+
+-(void)setupButton {
+	[self configureButtonLayer];
+}
+
+-(void)configureButtonLayer {
+	[self.layer insertSublayer:self.gradientLayer atIndex:0];
+	self.layer.cornerRadius = 7.0f;
+	self.layer.masksToBounds = YES;
+	self.layer.borderWidth = 0.0f;
+}
+
+-(void)updateButton {
+	if (self.highColor && self.lowColor) {
+        if ( self.highlighted ) {
+			[self.gradientLayer setColors:[NSArray arrayWithObjects:(id)[self.lowColor CGColor], (id)[self.highColor CGColor], nil]];
+		}
+		else {
+			[self.gradientLayer setColors:[NSArray arrayWithObjects:(id)[self.highColor CGColor], (id)[self.lowColor CGColor], nil]];
+		}
+    }
+}
+
+#pragma mark getters & setters
+-(CAGradientLayer *)gradientLayer {
+	if ( _gradientLayer == nil ) {
+		_gradientLayer = [[CAGradientLayer alloc] init];
+		_gradientLayer.bounds = [self bounds];
+		_gradientLayer.position = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+	}
+
+	return _gradientLayer;
+}
+
+-(void)setHighColor:(UIColor*)color {
+    _highColor = color;
+    [self updateButton];
+}
+
+- (void)setLowColor:(UIColor*)color {
+    _lowColor = color;
+    [self updateButton];
+}
+
+-(void)setHighlighted:(BOOL)highlighted {
+	[super setHighlighted:highlighted];
+	[self updateButton];
+}
+
+@end
